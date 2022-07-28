e3d3ed6d1cfa09379c33b1ef292cb48ba6521180
Ph <ph@Hs-MacBook-Pro.lan>
2022-07-28 16:55:11 +0100

    Ticket 9 in progress - adding comments

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
-import {getCommentsByArticleID} from '../api'
 
-const Comments = ({comment_count, article_id}) => {
-    const [comments, setComments]   = useState([])
-    const [loading, setLoading]     = useState(true)
-
-    useEffect(()=>{
-        getCommentsByArticleID(article_id)
-        .then((data)=>{
-            setLoading(false)
-            console.log(data.comments)
-            setComments(data.comments)
-        })
-    }, [])
-
-    return (<>
-        {loading && <div> Loading... </div> }
-        <div className="comment-count-holder">Comments: {comment_count}</div> 
-        <AddComment />
-        <div className="comment-card-holder">
-            {!loading && <div> 
-                
-                {comments.map((comment)=>{
-                    return(<>
-                    <div className="comment-card">  
+const AddComment = () => {
+    const { user, setUser } = useContext(UserContext);
+    return (
+        <div className="comment-card">  
                         <div className="comment-card-second-column">
                             <div className="comment-card-img"> </div>
                         </div>
@@ -40,14 +20,7 @@ const Comments = ({comment_count, article_id}) => {
                             </div>
                         </div>
                     </div>
-                        </>
-                    )
-                })}
-
-            </div> }
-        </div> 
-
-    </>)
+    )
 }
 
-export default Comments;
+export default AddComment;
